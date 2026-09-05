4acb7bef273abdb990c0468fef7e85198c14c914
Jhonatan Galindo <jhonatandgalindo@unicesar.edu.co>
2026-09-05 15:38:45 -0500

    feat: class Customer implemented as inherit of Person. Sale isnt implemented yet.

---
+++ src/main/java/com/gamezone/model/Customer.java
@@ -0,0 +1,30 @@
+package com.gamezone.model;
+
+import java.util.List;
+
+public class Customer extends Person{
+    private String mail;
+    private List<Sale> purchaseHistory;
+
+    public Customer(String name, String phone, String identification, String mail, List<Sale> purchaseHistory) {
+        super(name, phone, identification);
+        this.mail = mail;
+        this.purchaseHistory = purchaseHistory;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public List<Sale> getPurchaseHistory() {
+        return purchaseHistory;
+    }
+
+    public void addPurchaseHistory(Sale sale) {
+        purchaseHistory.add(sale);
+    }
+}
