c8748cef3e0178d31767b702e5ac27b273059717
Jhonatan Galindo <jhonatandgalindo@unicesar.edu.co>
2026-09-06 08:40:55 -0500

    fix: fixed error with the variable identification in Customer and Seller. Now called id.

--- src/main/java/com/gamezone/model/Customer.java
+++ src/main/java/com/gamezone/model/Customer.java
@@ -6,8 +6,8 @@ public class Customer extends Person{
     private String mail;
     private List<Sale> purchaseHistory;
 
-    public Customer(String name, String phone, String identification, String mail, List<Sale> purchaseHistory) {
-        super(name, phone, identification);
+    public Customer(String name, String phone, String id, String mail, List<Sale> purchaseHistory) {
+        super(name, phone, id);
         this.mail = mail;
         this.purchaseHistory = purchaseHistory;
     }
